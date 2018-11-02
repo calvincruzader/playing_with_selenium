fe19014367de81c943705a14b01cd9f9cffeb239
Calvin <calvincruzader@gmail.com>
2018-11-01 19:48:45 -0500

    Simple function to pull up Google in Selenium.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import drivers.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleDriver sd = new SimpleDriver();
+        sd.drive();
+    }
+}
---
+++ src/drivers/SimpleDriver.java
@@ -0,0 +1,24 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+package drivers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleDriver {
+    public SimpleDriver() {
+    }
+
+    public void drive() {
+        System.setProperty("webdriver.chrome.driver", "C:\\extrenal_libs\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(new CharSequence[]{"worlds 2018"});
+        searchBox.submit();
+        driver.quit();
+    }
+}
